6ab8b4133afa5658323aff458f6564ce731a8757
voy0 <piotrwojcik.wp@gmail.com>
2026-09-10 21:56:37 +0200

    deployment_version_1.0

--- FAFF/astro.config.mjs
+++ FAFF/astro.config.mjs
@@ -1,18 +1,23 @@
 import { defineConfig } from "astro/config";
 import react from '@astrojs/react';
-import keystatic from '@keystatic/astro';
+
+const integrations = [react()];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { default: keystatic } = await import('@keystatic/astro');
+  integrations.push(keystatic());
+}
 
 export default defineConfig({
+  site: 'https://voy0.github.io',
+  base: '/Fahie-Amusements-and-Fair-Foods',
   output: 'static',
   
   env: {
     schema: {},
   },
 
-  integrations: [
-    react(), 
-    keystatic()
-  ],
+  integrations,
 
   vite: {
     // REMOVED the ssr: external block so Astro can do its job!
